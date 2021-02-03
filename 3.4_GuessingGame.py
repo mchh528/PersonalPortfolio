6dd90ed4e6001df726d88fb84a642ab9cc19672a
Marlon Chhour <marlon1293@gmail.com>
2021-02-03 13:42:14 +1300

    Guessing Game
    - using random library to randomly generate a number from a specified range (1 - 100)
    - user attempts to guess the number generated
    - user is notified whether their guess is either too high or too low
    - returns the number of guesses taken to guess the correct number

---
+++ 3.4_GuessingGame.py
@@ -0,0 +1,37 @@
+import random
+
+num = random.randint(1, 100)
+print(num)
+count = 0;
+
+guess = 0;
+while (guess == 0):
+    try:
+        guess = int(input("Guess what number has the computer selected? "))
+    except ValueError:
+        print("Valid Answers only.")
+
+while (num != guess):
+    # If user's guess is greater than computer generated number, printing that the number guessed is too high
+    while (guess > num):
+        print("Too high..")
+        try:
+            guess = int(input("Guess what number has the computer selected? "))
+            count += 1;
+        except ValueError:
+            print("Valid Answers only.")
+
+    # If user's guess is less than computer generated number, printing that the number guessed is too low
+    while (guess < num):
+        print("Too low..")
+        try:
+            guess = int(input("Guess what number has the computer selected? "))
+            count += 1;
+        except ValueError:
+            print("Valid Answers only.")
+    
+# If user's guess matches the computer generated number, print congratulatory message and the number of guesses it took
+if (guess == num):
+    count += 1;
+    print("Congrats! You guessed the number! The number was {}".format(num))
+    print("You took {} guesses".format(count))
